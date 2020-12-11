288e4189b09d8f7ebd9bf4f07b4070ccfb7f202b
Ollie Backhouse <olbackhouse@gmail.com>
2020-12-11 10:32:29 +0000

    Fixes import trick when .swp files exist

--- adc2/methods.py
+++ adc2/methods.py
@@ -4,7 +4,7 @@ files = os.listdir(os.path.dirname(os.path.realpath(__file__)))
 __all__ = []
 
 for f in files:
-    if 'adc' in f and '.py' in f:
+    if 'adc' in f and f[-3:] == '.py':
         name = f[:-3]
         globals()[name] = importlib.import_module('adc2.%s' % name)
         __all__.append(name)
