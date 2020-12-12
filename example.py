e53b047931edeb2baa573f4127ae6891b618e0e2
Ollie Backhouse <olbackhouse@gmail.com>
2020-12-12 12:13:52 +0000

    Fixes example

--- example.py
+++ example.py
@@ -6,10 +6,10 @@ mol = gto.M(atom='O 0 0 0; O 0 0 1', basis='cc-pvtz', verbose=0)
 rhf = scf.RHF(mol).run(conv_tol=1e-12)
 
 t0 = time.time()
-e_ip = adc2.run(rhf, method='2', which='ip')[0]
-e_ea = adc2.run(rhf, method='2', which='ea')[0]
+e_ip = adc.run(rhf, method='2', which='ip')[0]
+e_ea = adc.run(rhf, method='2', which='ea')[0]
 t1 = time.time()
-if adc2.mpi_helper.rank == 0:
+if adc.mpi_helper.rank == 0:
     print('Exact ERIs:')
     print('IP:', e_ip)
     print('EA:', e_ea)
@@ -22,7 +22,7 @@ t0 = time.time()
 e_ip = adc.run(rhf, method='2', which='ip')[0]
 e_ea = adc.run(rhf, method='2', which='ea')[0]
 t1 = time.time()
-if adc2.mpi_helper.rank == 0:
+if adc.mpi_helper.rank == 0:
     print('Density fitting:')
     print('IP:', e_ip)
     print('EA:', e_ea)
