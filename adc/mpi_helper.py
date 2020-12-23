92515927eaa8e211c4721569e992ea9a0e7ef414
Ollie Backhouse <olbackhouse@gmail.com>
2020-12-23 12:59:07 +0000

    Lots of MPI fixes and utilities

--- adc/mpi_helper.py
+++ adc/mpi_helper.py
@@ -95,31 +95,82 @@ def distr_blocks(block):
         end = max(end, block)
     return start, end
 
+def allclose(a):
+    # numpy.allclose between MPI processes
+
+    a_in = a.copy()
+    a = allreduce(a_in) / size
+    
+    return np.allclose(a_in, a)
+
+@check_for_mpi
+def correct_vector_phase(v, full_check=False, extra=None):
+    # vectors may be out by a sign between MPI processes
+
+    if full_check:
+        el = v
+    else:
+        el = v.ravel()[[0]]
+
+    barrier()
+    root_el = bcast(el)
+
+    if np.linalg.norm(root_el + el) < np.linalg.norm(root_el - el):
+        v *= -1
+        if extra:
+            extra = [-x for x in extra]
+
+    if extra is None:
+        return v
+    else:
+        return (v,) + tuple(extra)
+
+@check_for_mpi
+def mean(m):
+    # take the mean of an array on each MPI process to gurantee determinism
+
+    barrier()
+    allreduce_inplace(m)
+    m /= size
+
+    return m
+
 def dot(a, b):
     # numpy.dot with MPI support
 
+    squeeze_left = squeeze_right = False
     if a.ndim == 1:
+        squeeze_left = True
         a = a.reshape(1, -1)
     elif a.ndim > 2:
         raise ValueError
-
     if b.ndim == 1:
+        squeeze_right = True
         b = b.reshape(-1, 1)
     elif b.ndim > 2:
         raise ValueError
 
     assert a.shape[1] == b.shape[0]
+    dtype = np.result_type(a.dtype, b.dtype)
     axes = (a.shape[0], a.shape[1], b.shape[0])
     choice = np.argmax(axes)
+
     p0, p1 = distr_blocks(axes[choice])
-    res = np.zeros((a.shape[0], b.shape[1]))
+    res = np.zeros((a.shape[0], b.shape[1]), dtype=dtype)
 
     if choice == 0:
-        res[p0:p1] = np.dot(a[p0:p1], b)
+        res[p0:p1] = np.dot(a[p0:p1], b, out=res[p0:p1])
     elif choice == 1:
-        res = np.dot(a[:,p0:p1], b[p0:p1])
+        res[:] = np.dot(a[:,p0:p1], b[p0:p1], out=res)
     else:
-        res[:,p0:p1] = np.dot(a, b[:,p0:p1])
+        res[:,p0:p1] = np.dot(a, b[:,p0:p1], out=res[:,p0:p1])
+
+    if squeeze_left and squeeze_right:
+        res = res.ravel()[0]
+    elif squeeze_left:
+        res = res.squeeze(axis=0)
+    elif squeeze_right:
+        res = res.squeeze(axis=1)
 
     barrier()
     allreduce_inplace(res)
@@ -185,11 +236,12 @@ def tensordot(a, b, axes=2):
 
 def einsum(key, a, b):
     # single numpy.einsum contraction via tensordot with MPI support
+    # the same axes must be contracted in a and b (order may vary)
 
     akey, bkey = key.split('->')[0].split(',')
     rkey = key.split('->')[1]
-
     rkey_default = ''
+
     axes_a = []
     axes_b = []
 
@@ -207,6 +259,14 @@ def einsum(key, a, b):
         else:
             axes_b.append(i)
 
+    if len(axes_a) != len(axes_b):
+        raise NotImplementedError
+
+    keys_to_sum_a = ''.join([akey[x] for x in axes_a])
+    keys_to_sum_b = ''.join([bkey[x] for x in axes_b])
+    if keys_to_sum_a != keys_to_sum_b:
+        axes_b = [axes_b[keys_to_sum_a.index(k)] for k in keys_to_sum_b]
+
     res = tensordot(a, b, axes=(tuple(axes_a[::-1]), tuple(axes_b[::-1])))
 
     if rkey != rkey_default:
@@ -215,3 +275,21 @@ def einsum(key, a, b):
 
     return res
 
+
+if __name__ == '__main__':
+    keys = [
+        'ibjc,jia->cab',
+        'cab,icab->i',
+        'ibjc,jia->cab',
+        'cab,icab->i',
+        'i,ibac->cab',
+        'cab,jcib->ija',
+    ]
+    sizes = dict(i=5, j=5, k=5, l=5, a=5, b=5, c=5, d=5)
+
+    for key in keys:
+        a = np.random.random([sizes[x] for x in key.split(',')[0]])
+        b = np.random.random([sizes[x] for x in key.split(',')[1].split('->')[0]])
+        resa = np.einsum(key, a, b)
+        resb =    einsum(key, a, b)
+        print('%16s %8s' % (key, np.allclose(resa, resb)))
